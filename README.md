ede166fd551b4bf7c9b813749898ad297f40df79
ZhangDong <zhangd1@joywise.net>
2018-10-08 13:20:28 +0800

    自定义模板引擎

--- README.md
+++ README.md
@@ -1,16 +1,6 @@
 fis3-preprocessor-html-inline
 ===============
-简易html资源动态引入处理，支持引入参数替换变量
-
-index.html
-```
-<link rel="import" href="test.html?__inline&test=123">
-```
-
-test.html
-```
-test: #{test}
-```
+简易html资源动态引入处理，支持引入参数替换变量，支持自定义模板渲染引擎
 
 ## 使用
 ```
@@ -19,11 +9,101 @@ npm install fis3-preprocessor-html-inline
 
 ```js
 fis.match('*.html', {
-  preprocessor: fis.plugin('html-inline')
+	preprocessor: fis.plugin('html-inline')
 })
 ```
 
 ## 参数说明
+- `compile`: `null` 自定义模板渲染引擎，为`null`则使用简易变量替换，配合下方参数使用
+- `ld`: `{{` 语法标签（左）
+- `rd`: `}}` 语法标签（右）
+- `removeEmpty`: `true` 是否删除无数据变量
 
-- `ld`: 语法标签（左），默认为 `#{`
-- `rd`: 语法标签（右），默认为 `}`
+## 自定义模板渲染引擎
+
+以`art-template`为例：
+```js
+const template = require('art-template');
+fis.match('*.html', {
+	preprocessor: fis.plugin('html-inline', {
+		compile(target, tpl, data) {
+			const id = target.moduleId
+			if(!template.cache[id] || !target.useCache || template.cache[id + '.cache']!= target.cache.timestamp) {
+				template.cache[id] = template.compile(tpl)
+				template.cache[id + '.cache'] = target.useCache ? target.cache.timestamp : +new Date()
+			}
+
+			return template(id, data)
+		}
+	})
+})
+```
+
+> 暂不支持 art-template的include语法
+
+## 参数数据载入规则
+
+### query载入变量
+index.html
+```html
+<link rel="import" href="test.html?__inline&test=123&user[0]=东东&user[1]=丽丽">
+```
+
+test.html
+```html
+test: {{test}}
+user: {{user}}
+user0: {{user[0]}}
+```
+
+结果
+```html
+test: 123
+user: 东东,丽丽
+user0: 东东
+```
+
+> 暂时只支持标准参数、数组参数
+
+### __inline载入变量
+index.html
+```html
+<link rel="import" href="user.html?__inline={name: '丽丽', age: 18}">
+```
+
+user.html
+```html
+<p>你好 {{name}}，你今年<em>{{age}}</em>岁咯！加油！</p>
+```
+
+结果
+```html
+<p>你好 丽丽，你今年<em>18</em>岁咯！加油！</p>
+```
+
+### __inline载入json
+
+
+```html
+<link rel="import" href="user.html?__inline=user.json">
+```
+
+user.html
+```html
+<p>你好 {{name}}，你今年<em>{{age}}</em>岁咯！加油！</p>
+```
+
+user.json
+```json
+{
+    "name": "丽丽",
+    "age": 18
+}
+```
+
+结果
+```html
+<p>你好 丽丽，你今年<em>18</em>岁咯！加油！</p>
+```
+
+> json文件路径查找顺序：当前文件、模板文件
--- index.js
+++ index.js
@@ -1,43 +1,70 @@
-var lang = fis.compile.lang;
-var fs = require('fs');
-var reg = /<!--\s*inline\[([^\]]+)\]\s*-->|<!--([\s\S]*?)-->|<link\s+([\s\S]*?["'\s\w\/\-])(?:>|$)/ig,
+const extend = require('extend')
+const reg = /<!--\s*inline\[([^\]]+)\]\s*-->|<!--([\s\S]*?)-->|<link\s+([\s\S]*?["'\s\w\/\-])(?:>|$)/ig,
 	rRel = /\srel\s*=\s*('[^']+'|"[^"]+"|[^\s\/>]+)/i,
 	rHref = /(\s(?:data-)?href\s*=\s*)('[^']+'|"[^"]+"|[^\s\/>]+)/i,
 	rQuote = /^['"]|['"]$/g,
-	rRegCover = /([$*+.?\\^|()[\]{}])/g;
+	rLinkEnd = /\/(?=>$)/,
+	rLinkAttr = /\s+(?:charset|href|data-href|hreflang|rel|rev|sizes|target)\s*=\s*(?:'[^']+'|"[^"]+"|[^\s\/>]+)/ig,
+	rRegCover = /([$*+.?\\^|()[\]{}])/g,
+	rJSON = /^[{[\]]/,
+	rJSONFile = /\.json$/;
+
+/**
+ * 验证是否数组
+ *
+ * @param {Array} arr
+ * @return {Boolean}
+ */
+function isArray(arr) {
+	return Object.prototype.toString.apply(arr) === '[object Array]'
+}
+
+/**
+ * 添加依赖
+ *
+ * @param {Object} a
+ * @param {Object} b
+ */
+function addDeps(a, b) {
+	if (a && a.cache && b) {
+		if (b.cache) {
+			a.cache.mergeDeps(b.cache);
+		}
+		a.cache.addDeps(b.realpath || b);
+	}
+}
 
 /**
  * url参数转换为Object
  *
+ * @description 只支持正常参数、数组参数，如：?test=123&opts[0]=a&opts[1]=b
+ *
  * @param {String} query 需要解析的query
  * @returns {Object}
  */
-var paramAryReg = /(.*?)\[(\d+)\]/;
+const paramAryReg = /(.*?)\[(\d+)\]/;
 function parseQuery(query) {
 	let params = {}
 
 	if(!query) return params
 
 	query = query.substring(query.indexOf('?') + 1)
-	var queryAry = query.split('&')
+	const queryAry = query.split('&')
 
 	queryAry.forEach(item => {
 		let param = item.split('=')
 		if(!param[0]) return
 
 		param[1] = decodeURIComponent(param[1])
-		var aryMatch = param[0].match(paramAryReg)
+		const aryMatch = param[0].match(paramAryReg)
 		if (aryMatch) {
 			if (typeof params[aryMatch[1]] == 'undefined') {
 				params[aryMatch[1]] = []
 			}
 			params[aryMatch[1]][aryMatch[2]] = param[1]
 		} else {
-			if (params[param[0]]) {
-				params[param[0]] = params[param[0]] + ',' + param[1]
-			} else {
-				params[param[0]] = param[1]
-			}
+			let curParam = params[param[0]]
+			params[param[0]] = (curParam ? curParam+',' : '') + param[1]
 		}
 	})
 
@@ -45,19 +72,79 @@ function parseQuery(query) {
 }
 
 /**
- * 判断info.query是否为inline
+ * 解析参数
  *
- * - `abc?__inline` return true
- * - `abc?__inlinee` return false
- * - `abc?a=1&__inline'` return true
- * - `abc?a=1&__inline=` return true
- * - `abc?a=1&__inline&` return true
- * - `abc?a=1&__inline` return true
- * @param {Object} info
- * @memberOf fis.compile
+ * @param {Object} params 待处理参数，__inline参数可携带JSON对象、json文件路径
+ * @param {fis.FILE} file 当前文件对象，用于查找json文件
+ * @param {fis.FILE} target 目标文件对象，用于查找json文件
+ * @returns {Object}
  */
-function isInline(info) {
-	return /[?&]__inline(?:[=&'"]|$)/.test(info.query);
+function parseParam(params, file, target) {
+	let inlineData = {},
+		inlineValue = params['__inline'];
+
+	delete params['__inline']
+
+	// __inline=data.json|{"test": 123}
+	if(inlineValue) {
+		// 尝试解析JSON
+		if(rJSON.test(inlineValue)) {
+			let tmpData
+			// try {
+				// tmpData = JSON.parse(inlineValue)
+			// }catch(e) {
+				// JSON解析失败，尝试使用function返回
+				try {
+					tmpData = new Function(`return ${inlineValue}`)()
+				}catch(e){
+					// 并不是个JSON
+				}
+			// }
+			if(typeof tmpData=='object' && tmpData) {
+				inlineData = tmpData
+			}
+		}else if(rJSONFile.test(inlineValue)) {
+			// 尝试从当前文件下、目标文件下查找json文件数据源
+			// NOTE: 使用lookup api，会触发hook-node_modules文件查找
+			// let jsonFile = fis.project.lookup(inlineValue, file).file || fis.project.lookup(inlineValue, target).file
+			// let jsonFile = fis.uri(inlineValue, target ? target.dirname : fis.getProjectPath());
+			let jsonFile = fis.uri(inlineValue, file.dirname).file || fis.uri(inlineValue, target.dirname).file;
+			if(jsonFile) {
+				inlineData = fis.util.readJSON(jsonFile.realpath)
+			}
+		}
+	}
+
+	// 如果json是数组，则忽略url带的参数，因为extend后，数组会转换成对象
+	return isArray(inlineData) ? inlineData : extend(true, {}, inlineData, params)
+}
+
+/**
+ * 简易编译，只替换参数变量，不支持其他任何语法
+ *
+ * @description 姓名：{{name}}，年龄：{{age}}
+ *
+ * @param {String} content 编译内容
+ * @param {*} data 编译数据源
+ * @param {*} options 选项
+ * @returns
+ */
+function compileSimple(target, content, data, options) {
+	var regStr = options.ld.replace(rRegCover, '\\$1') + '\\s*(\\w+)([[.].*?)?\\s*' + options.rd.replace(rRegCover, '\\$1')
+	var rContent = new RegExp(regStr, 'g')
+
+	return content.replace(rContent, (m, key, path) => {
+		let val = new Function(`return this['${key}']${path ? path : ''}`).apply(data),
+		def = options.removeEmpty ? '' : m
+
+		return val ? val : def
+	})
+}
+
+function compile(target, content, data, options) {
+	var enigen = options.compile || compileSimple
+
+	return enigen.apply(null, arguments)
 }
 
 module.exports = function(content, file, options) {
@@ -66,53 +153,70 @@ module.exports = function(content, file, options) {
 		return content;
 	}
 
-	var regStr = options.ld.replace(rRegCover, '\\$1') + '(\\w+?)' + options.rd.replace(rRegCover, '\\$1')
-	var rContent = new RegExp(regStr, 'g')
-
-	var res = content.replace(reg, (m, commentInline, comment, link) => {
+	return content.replace(reg, (m, commentInline, comment, link) => {
 		if (!commentInline && !link) return m;
 
-		var path = ''
+		let path = '',
+			isCssLink = false,
+			isImportLink = false;
+
 		// <!--inline[]-->
 		if(commentInline) {
 			path = commentInline
 		}else if(link) {
 			// <link rel="import|stylesheet">
-			var result = m.match(rRel);
+			const result = m.match(rRel);
 			if (result && result[1]) {
-				var rel = result[1].replace(rQuote, '').toLowerCase();
-				if(rel !== 'import' && rel !== 'stylesheet') return m
+				const rel = result[1].replace(rQuote, '').toLowerCase();
+				isCssLink = rel === 'stylesheet';
+				isImportLink = rel === 'import';
+
+				if(!isCssLink && !isImportLink) return m
 			}
 
-			var pathMatch = m.match(rHref);
+			const pathMatch = m.match(rHref);
 			if(pathMatch && pathMatch[2]) {
 				path = pathMatch[2].replace(rQuote, '')
 			}
 		}
 
-		var queryInfo = fis.util.query(path)
-		if(!isInline(queryInfo)) return m
-		var query = queryInfo.query.replace('__inline', '')
-		var params = parseQuery(query)
-		if(JSON.stringify(params)=='{}') return m
+		// 解析参数，无__inline参数值，且无任何其他参数，不处理
+		const query = parseQuery(fis.util.query(path).query)
+		if(Object.keys(query).length<=1 && !query['__inline']) return m
 
-		var info = fis.project.lookup(path, file);
-		var target = info.file;
-		if(!target.isText()) return m
+		// 获取目标文件对象
+		const target = fis.project.lookup(path, file).file;
+		if(!target || !target.isFile() || !target.isText()) return m
 
+		// 解析参数
+		const params = parseParam(query, file, target)
+
+		// 先编译目标文件（fis.match配置的编译）、加入依赖表
 		fis.compile(target);
-		var inlineContent = target.getContent().replace(rContent, (m, key) => {
-			return params[key] ? params[key] : m
-		})
+		file.addLink(target.subpath);
+		addDeps(file, target);
 
+		// 二次编译，将参数带到文件里
+		let inlineContent = ''
+		if (isCssLink) {
+			inlineContent += '<style' + m.substring(5).replace(rLinkEnd, '').replace(rLinkAttr, '');
+		}
+		inlineContent += compile(target, target.getContent(), params, options)
+		if (isCssLink) {
+			inlineContent += '</style>';
+		}
+
+		// return inlineContent || m;
 		return inlineContent;
 	})
-
- 	return res
 };
 
 
 module.exports.defaultOptions = {
-	ld: '#{',
-	rd: '}'
+	// 默认使用自带简易编译，配合ld、rd语法标识参数
+	// 这里可以设置为任何模板引擎，自行实现
+	compile: null,
+	ld: '{{',
+	rd: '}}',
+	removeEmpty: true
 }
